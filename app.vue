2b3c8e145d461fa21f2aeba3a5a56c9990f41fa4
LucasCota <lucas.a.cota@gmail.com>
2023-11-13 13:37:44 -0300

    sharing data between components

--- app.vue
+++ app.vue
@@ -4,9 +4,17 @@
     <div class="container">
       <Balance />
       <IncomeExpenses />
-      <TransactionList />
+      <TransactionList :transactions="transactions"/>
       <AddTransactions />
     </div>
   </div>
 </template>
 
+<script setup lang="ts">
+
+  const transactions = ref([
+    {id:1, text: 'Flower', amount: -19.99},
+    {id:2, text: 'Salary', amount: 299.97},
+    {id:3, text: 'Book', amount: -10},
+  ])
+</script>
--- components/TransactionList.vue
+++ components/TransactionList.vue
@@ -2,11 +2,13 @@
   <div>
     <h3>History</h3>
     <ul id="list" class="list">
-      <li v-for="transaction in transactions" 
+      <li 
+        v-for="transaction in transactions" 
         :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
       >
          {{ transaction.text }}
-        <span> {{ transaction.amount }}</span>
+        <span>$ {{ transaction.amount }}</span>
         <button class="delete-btn">x</button>
       </li>
     </ul>
@@ -14,9 +16,17 @@
 </template>
 
 <script lang="ts" setup>
-  const transactions = [
-    {id:1, text: 'Flower', amount: -19.99},
-    {id:2, text: 'Salary', amount: 299.97},
-    {id:3, text: 'Book', amount: -10},
-  ]
+  interface Transactions {
+    id:number,
+    text: string,
+    amount: number
+  }
+  
+  defineProps({
+    transactions: 
+      {
+        type: Array as() => Array<Transactions>,
+        required: true
+      } 
+ })
 </script>
