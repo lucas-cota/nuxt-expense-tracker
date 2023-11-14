363560b876346fe1359321701c792df1cb164c83
LucasCota <lucas.a.cota@gmail.com>
2023-11-14 10:16:30 -0300

    binding form inputs

--- components/AddTransactions.vue
+++ components/AddTransactions.vue
@@ -1,15 +1,24 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input type="text" id="text" v-model="text" placeholder="Enter text..." />
     </div>
     <div class="form-control">
       <label for="amount">Amount <br />
         (negative - expense, positive - income)</label>
-      <input type="number" id="amount" placeholder="Enter amount..." />
+      <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
+
+<script lang="ts" setup>
+const text = ref('')
+const amount = ref('')
+
+const onSubmit = () => {
+  console.log(text.value, amount.value)
+}
+</script>
